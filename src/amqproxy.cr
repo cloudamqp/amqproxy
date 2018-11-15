114eea91dc13ee51bd0d1c4f545c24e5026db5c5
Carl Hörberg <carl.hoerberg@gmail.com>
2018-11-15 16:31:58 +0100

    better debian package

--- src/amqproxy.cr
+++ src/amqproxy.cr
@@ -11,7 +11,8 @@ p = OptionParser.parse! do |parser|
   parser.on("-l ADDRESS", "--listen=ADDRESS", "Address to listen on (default is all)") { |p| listen_address = p }
   parser.on("-p PORT", "--port=PORT", "Port to listen on (default: 5673)") { |p| listen_port = p.to_i }
   parser.on("-d", "--debug", "Verbose logging") { |d| log_level = Logger::DEBUG }
-  parser.on("-h", "--help", "Show this help") { abort parser.to_s }
+  parser.on("-h", "--help", "Show this help") { puts parser.to_s; exit 0 }
+  parser.on("-v", "--version", "Display version") { puts AMQProxy::VERSION.to_s; exit 0 }
   parser.invalid_option { |arg| abort "Invalid argument: #{arg}" }
 end
 
