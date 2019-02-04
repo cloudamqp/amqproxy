413454ac0ef8047c582934d43748e760be51684b
Carl Hörberg <carl.hoerberg@gmail.com>
2019-02-04 14:05:36 +0100

    use sudo on travis ci to rabbitmqctl

--- spec/amqproxy_spec.cr
+++ spec/amqproxy_spec.cr
@@ -19,15 +19,16 @@ describe AMQProxy::Server do
   end
 
   it "can reconnect if upstream closes" do
+    sudo = ENV["TRAVIS"]? ? "sudo" : ""
     system "while (nc localhost 5673 -w 1); do sleep 1; done"
     s = AMQProxy::Server.new("127.0.0.1", 5672, false, Logger::ERROR)
     spawn { s.listen("127.0.0.1", 5673) }
     sleep 0.001
     AMQP::Client.start("amqp://localhost:5673") do |conn|
       conn.channel
-      system "rabbitmqctl stop_app > /dev/null"
+      system "#{sudo} rabbitmqctl stop_app > /dev/null"
     end
-    system "rabbitmqctl start_app > /dev/null"
+    system "#{sudo} rabbitmqctl start_app > /dev/null"
     AMQP::Client.start("amqp://localhost:5673") do |conn|
       conn.channel
       s.client_connections.should eq(1)
