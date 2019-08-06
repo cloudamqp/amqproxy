68a72043f842c3a9608514e48d5c19ea17644669
Carl Hörberg <carl.hoerberg@gmail.com>
2019-08-06 15:21:27 +0200

    shrink pool size every 60s

--- src/amqproxy/pool.cr
+++ src/amqproxy/pool.cr
@@ -4,6 +4,7 @@ module AMQProxy
     def initialize(@host : String, @port : Int32, @tls : Bool, @log : Logger)
       @pools = {} of String => Deque(Upstream)
       @size = 0
+      spawn shrink_loop
     end
 
     def borrow(user : String, password : String, vhost : String, &block : (Upstream | Nil) -> _)
@@ -22,6 +23,26 @@ module AMQProxy
         @log.error "Upstream connection closed when returned"
       elsif !q.nil?
         q.push u
+      end
+    end
+
+    private def shrink_loop
+      loop do
+        sleep 60
+        shrink
+      end
+    end
+
+    private def shrink
+      @pools.each_value do |q|
+        while u = q.shift?
+          @size -= 1
+          begin
+            u.close
+          rescue ex
+            @log.error "Problem closing upstream: #{ex.inspect}"
+          end
+        end
       end
     end
   end
