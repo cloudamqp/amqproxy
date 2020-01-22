830d6b155f59b472ebee8062ce56c3f3efdcb4e3
Carl Hörberg <carl.hoerberg@gmail.com>
2020-01-22 13:23:38 +0100

    test app for many connections

---
+++ test/many_conns.cr
@@ -0,0 +1,24 @@
+require "amqp-client"
+
+abort "Usage: #{PROGRAM_NAME} <amqp-url> <client-count> <msg-count-per-client>" if ARGV.size != 3
+url, conns, msgs = ARGV
+puts "Publishing #{msgs} msgs on #{conns} connections"
+done = Channel(Nil).new
+conns.to_i.times do |idx|
+  spawn do
+    AMQP::Client.start(url) do |c|
+      c.channel do |ch|
+        q = ch.queue("test")
+        msgs.to_i.times do |idx|
+          q.publish "msg #{idx}"
+        end
+      end
+    end
+  ensure
+    done.send nil
+  end
+end
+
+conns.to_i.times do
+  done.receive?
+end
