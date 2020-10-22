816f39b751cbb14e3012c11599a148e9f6caa40d
SchizoDuckie <SchizoDuckie@users.noreply.github.com>
2020-10-22 21:23:56 +0200

    Include timestamps for logging when in debug mode
    This commit introduces timestamps in the log fomatter output when starting amqproxy in debug mode, making diagnosing intermittent connection problems easier.

--- src/amqproxy/server.cr
+++ src/amqproxy/server.cr
@@ -13,8 +13,14 @@ module AMQProxy
     def initialize(upstream_host, upstream_port, upstream_tls, log_level = Logger::INFO)
       @log = Logger.new(STDOUT)
       @log.level = log_level
-      @log.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
-        io << "\r" << message
+      if(@log.level == Logger::DEBUG)
+        @log.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
+          io << "\r" << datetime << " : " << message
+        end
+      else
+        @log.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
+          io << "\r" << message
+        end
       end
       @clients = Array(Client).new
       @pool = Pool.new(upstream_host, upstream_port, upstream_tls, @log)
