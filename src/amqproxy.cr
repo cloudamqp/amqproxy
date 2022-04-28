cb7ae32d58c424bff3b9f6c3e2a3577528c0d852
Carl Hörberg <carl@84codes.com>
2022-04-28 11:09:46 +0200

    Support for config files
    Fixes #64

--- src/amqproxy.cr
+++ src/amqproxy.cr
@@ -2,45 +2,86 @@ require "./amqproxy/version"
 require "./amqproxy/server"
 require "option_parser"
 require "uri"
+require "ini"
+require "logger"
 
-listen_address = ENV["LISTEN_ADDRESS"]? || "localhost"
-listen_port = ENV["LISTEN_PORT"]? || 5673
-log_level = Logger::INFO
-idle_connection_timeout = 5
-p = OptionParser.parse do |parser|
-  parser.banner = "Usage: amqproxy [options] [amqp upstream url]"
-  parser.on("-l ADDRESS", "--listen=ADDRESS", "Address to listen on (default is localhost)") { |p| listen_address = p }
-  parser.on("-p PORT", "--port=PORT", "Port to listen on (default: 5673)") { |p| listen_port = p.to_i }
-  parser.on("-t IDLE_CONNECTION_TIMEOUT", "--idle-connection-timeout=SECONDS", "Maxiumum time in seconds an unused pooled connection stays open (default 5s)") do |p|
-    idle_connection_timeout = p.to_i
+class AMQProxy::CLI
+  @listen_address = ENV["LISTEN_ADDRESS"]? || "localhost"
+  @listen_port = ENV["LISTEN_PORT"]? || 5673
+  @log_level : Logger::Severity = Logger::INFO
+  @idle_connection_timeout = 5
+  @upstream = ENV["AMQP_URL"]?
+
+  def parse_config(path)
+    INI.parse(File.read(path)).each do |name, section|
+      case name
+      when "main", ""
+        section.each do |key, value|
+          case key
+          when "upstream"                then @upstream = value
+          when "log_level"               then @log_level = Logger::Severity.parse(value)
+          when "idle_connection_timeout" then @idle_connection_timeout = value.to_i
+          else                                raise "Unsupported config #{name}/#{key}"
+          end
+        end
+      when "listen"
+        section.each do |key, value|
+          case key
+          when "port"            then @listen_port = value
+          when "bind", "address" then @listen_address = value
+          when "log_level"       then @log_level = Logger::Severity.parse(value)
+          else                        raise "Unsupported config #{name}/#{key}"
+          end
+        end
+      else raise "Unsupported config section #{name}"
+      end
+    end
+  rescue ex
+    abort ex.message
   end
-  parser.on("-d", "--debug", "Verbose logging") { |d| log_level = Logger::DEBUG }
-  parser.on("-h", "--help", "Show this help") { puts parser.to_s; exit 0 }
-  parser.on("-v", "--version", "Display version") { puts AMQProxy::VERSION.to_s; exit 0 }
-  parser.invalid_option { |arg| abort "Invalid argument: #{arg}" }
+
+  def run
+    p = OptionParser.parse do |parser|
+      parser.banner = "Usage: amqproxy [options] [amqp upstream url]"
+      parser.on("-l ADDRESS", "--listen=ADDRESS", "Address to listen on (default is localhost)") do |v|
+        @listen_address = v
+      end
+      parser.on("-p PORT", "--port=PORT", "Port to listen on (default: 5673)") { |v| @listen_port = v.to_i }
+      parser.on("-t IDLE_CONNECTION_TIMEOUT", "--idle-connection-timeout=SECONDS", "Maxiumum time in seconds an unused pooled connection stays open (default 5s)") do |v|
+        @idle_connection_timeout = v.to_i
+      end
+      parser.on("-d", "--debug", "Verbose logging") { @log_level = Logger::DEBUG }
+      parser.on("-c FILE", "--config=FILE", "Load config file") { |v| parse_config(v) }
+      parser.on("-h", "--help", "Show this help") { puts parser.to_s; exit 0 }
+      parser.on("-v", "--version", "Display version") { puts AMQProxy::VERSION.to_s; exit 0 }
+      parser.invalid_option { |arg| abort "Invalid argument: #{arg}" }
+    end
+
+    @upstream ||= ARGV.shift?
+    abort p.to_s if @upstream.nil?
+
+    u = URI.parse @upstream.not_nil!
+    abort "Invalid upstream URL" unless u.host
+    default_port =
+      case u.scheme
+      when "amqp"  then 5672
+      when "amqps" then 5671
+      else              abort "Not a valid upstream AMQP URL, should be on the format of amqps://hostname"
+      end
+    port = u.port || default_port
+    tls = u.scheme == "amqps"
+
+    server = AMQProxy::Server.new(u.host || "", port, tls, @log_level, @idle_connection_timeout)
+
+    shutdown = ->(_s : Signal) do
+      server.close
+      exit 0
+    end
+    Signal::INT.trap &shutdown
+    Signal::TERM.trap &shutdown
+
+    server.listen(@listen_address, @listen_port.to_i)
+  end
 end
 
-upstream = ARGV.shift? || ENV["AMQP_URL"]?
-abort p.to_s if upstream.nil?
-
-u = URI.parse upstream
-abort "Invalid upstream URL" unless u.host
-default_port =
-  case u.scheme
-  when "amqp" then 5672
-  when "amqps" then 5671
-  else abort "Not a valid upstream AMQP URL, should be on the format of amqps://hostname"
-  end
-port = u.port || default_port
-tls = u.scheme == "amqps"
-
-server = AMQProxy::Server.new(u.host || "", port, tls, log_level, idle_connection_timeout)
-
-shutdown = -> (s : Signal) do
-  server.close
-  exit 0
-end
-Signal::INT.trap &shutdown
-Signal::TERM.trap &shutdown
-
-server.listen(listen_address, listen_port.to_i)
+AMQProxy::CLI.new.run
