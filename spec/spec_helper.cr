852d3dca64a997376f0e9f45e0eb37a7c6ee99de
Carl Hörberg <carl@84codes.com>
2022-11-26 20:37:39 +0100

    sudo only if id -u isn't 0

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,4 +3,4 @@ require "../src/amqproxy/server"
 require "../src/amqproxy/version"
 require "amqp-client"
 
-MAYBE_SUDO = ENV["USER"]? == "root" ? "" : "sudo "
+MAYBE_SUDO = `id -u` == "0\n" ? "" : "sudo "
