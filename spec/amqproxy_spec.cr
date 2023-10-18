cloudamqp/amqproxy

commit 3e6e7cce002f813c02f6b91c9ea3da4c39b5ef5d

Disconnect clients on broken upstream connection (#128)
This change also removes the code added in https://github.com/cloudamqp/amqproxy/pull/104

The motivation behind that is

* IO:_Error that happens in Upstream#connect is raised as Upstream::Error
* IO::Error that happens in Upstream#read_loop is rescued in that method
* IO::Error that happens in Client#read_loop is raised as Client::Error

Adds an integration test with the php-amqp client run via Docker Compose
and Toxiproxy.

Close https://github.com/cloudamqp/amqproxy/issues/118
Close https://github.com/cloudamqp/amqproxy/issues/111
Close https://github.com/cloudamqp/amqproxy/issues/98

.github/workflows/ci.yml
@@ -7,6 +7,7 @@ on:
       - 'shard.lock'
       - 'src/**'
       - 'spec/**'
+      - 'test/**'
 
 jobs:
   spec:
@@ -45,3 +46,12 @@ jobs:
     steps:
       - uses: actions/checkout@v3
       - run: crystal tool format --check
+
+  integration:
+    runs-on: ubuntu-latest
+    timeout-minutes: 10
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v4
+      - name: PHP client integration test using Docker Compose
+        run: ./test/integration-php.sh

src/amqproxy/upstream.cr
@@ -40,6 +40,7 @@ module AMQProxy
     # Frames from upstream (to client)
     def read_loop # ameba:disable Metrics/CyclomaticComplexity
       socket = @socket
+      client = @current_client
       loop do
         AMQ::Protocol::Frame.from_io(socket, IO::ByteFormat::NetworkEndian) do |frame|
           case frame
@@ -55,7 +56,7 @@ module AMQProxy
             write frame
             next
           end
-          if client = @current_client
+          if client
             begin
               client.write(frame)
             rescue ex
@@ -84,6 +85,7 @@ module AMQProxy
       @log.error "Error reading from upstream: #{ex.inspect_with_backtrace}" unless @socket.closed?
     ensure
       @socket.close unless @socket.closed?
+      client.close_socket if client
     end
 
     SAFE_BASIC_METHODS = {40, 10} # qos and publish

test/integration-php/docker-compose.yml
@@ -1,0 +1,57 @@
+version: "3.8"
+configs:
+  toxiproxy_config:
+    file: ./toxiproxy.json
+services:
+  amqproxy:
+    build: ../../
+    expose:
+      - "5673"
+    entrypoint: ["amqproxy", "--debug", "--listen=0.0.0.0", "amqp://toxiproxy:7777"]
+    depends_on:
+      toxiproxy:
+        condition: service_started
+
+  toxiproxy:
+    image: ghcr.io/shopify/toxiproxy:latest
+    expose:
+      - "8474" # Toxiproxy HTTP API
+      - "7777" # Expose AMQP broker on this port
+    command: ["-host=0.0.0.0", "-config=/toxiproxy_config"]
+    configs:
+      - toxiproxy_config
+    depends_on:
+      rabbitmq:
+        condition: service_healthy
+
+  rabbitmq:
+    image: rabbitmq:latest
+    expose:
+      - "5672"
+    healthcheck:
+      test: ["CMD-SHELL", "rabbitmqctl status"]
+      interval: 2s
+      retries: 10
+      start_period: 20s
+
+  php-amqp:
+   build: ./php-amqp
+   environment:
+     TEST_RABBITMQ_HOST: amqproxy
+     TEST_RABBITMQ_PORT: 5673
+   # Makes this container exit with error if the test script hangs (timeout)
+   command: ["timeout", "15s", "php", "-d", "extension=amqp.so", "get-test.php"]
+   depends_on:
+     amqproxy:
+      condition: service_started
+
+  toxiproxy-cli:
+    # Need to build our own image as the toxiproxy image has no shell
+    build: ./toxiproxy-cli
+    environment:
+      TOXIPROXY_URL: "http://toxiproxy:8474"
+    # The PHP client needs to start and connect before we add the toxiproxy toxic
+    # that will break the amqproxy upstream connection, depends_on makes this happen
+    depends_on:
+      php-amqp:
+        condition: service_started

test/integration-php/php-amqp/Dockerfile
@@ -1,0 +1,21 @@
+FROM php:8.2-cli
+WORKDIR /app
+
+RUN apt-get update -q \
+  && apt-get install -qq cmake libssl-dev git unzip \
+  && rm -rf /var/lib/apt/lists/*
+
+# Install librabbitmq (https://github.com/alanxz/rabbitmq-c)
+RUN \
+  curl --location --silent --output /tmp/rabbitmq-c.tar.gz https://github.com/alanxz/rabbitmq-c/archive/v0.10.0.tar.gz \
+  && mkdir -p /tmp/rabbitmq-c/build \
+  && tar --gunzip --extract --strip-components 1 --directory /tmp/rabbitmq-c --file /tmp/rabbitmq-c.tar.gz \
+  && cd /tmp/rabbitmq-c/build \
+  && cmake -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_TOOLS=OFF -DENABLE_SSL_SUPPORT=ON .. \
+  && cmake --build . --target install \
+  && ln -s /usr/local/lib/x86_64-linux-gnu/librabbitmq.so.4 /usr/local/lib/
+
+# Install php-amqp (https://github.com/php-amqp/php-amqp)
+RUN echo /usr/local | pecl install amqp
+
+COPY *.php ./

test/integration-php/toxiproxy-cli/Dockerfile
@@ -1,0 +1,5 @@
+FROM alpine:latest
+COPY --from=ghcr.io/shopify/toxiproxy:latest ./toxiproxy-cli /toxiproxy-cli
+COPY ./entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]

test/integration-php/toxiproxy-cli/entrypoint.sh
@@ -1,0 +1,15 @@
+#!/bin/sh
+
+set -x
+
+/toxiproxy-cli toxic \
+  add \
+  --type reset_peer \
+  --attribute timeout=2000 \
+  --toxicName reset_peer between-proxy-and-broker
+
+echo "Toxic added, sleeping..."
+
+# We sleep so the container keeps running as otherwise all containers would stop
+# as we are using --exit-code-from (implies --abort-on-container-exit)
+sleep 900

src/amqproxy/server.cr
@@ -86,10 +86,6 @@ module AMQProxy
         close = AMQ::Protocol::Frame::Connection::Close.new(403_u16, "UPSTREAM_ERROR", 0_u16, 0_u16)
         close.to_io socket, IO::ByteFormat::NetworkEndian
         socket.flush
-      rescue ex : IO::Error
-        @log.error { "IO Error for user '#{user}' to vhost '#{vhost}': #{ex.message}" }
-        close = AMQ::Protocol::Frame::Connection::Close.new(403_u16, "IO_ERROR", 0_u16, 0_u16)
-        close.to_io socket, IO::ByteFormat::NetworkEndian
       end
     rescue ex : Client::Error
       @log.debug { "Client disconnected: #{remote_address}: #{ex.inspect}" }

test/integration-php.sh
@@ -1,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# --force-recreate and --renew-anon-volumes needed to start fresh every time
+# otherwise broker datadir volume may be re-used
+
+docker compose \
+  --file test/integration-php/docker-compose.yml \
+  up \
+  --remove-orphans \
+  --force-recreate \
+  --renew-anon-volumes \
+  --build \
+  --exit-code-from php-amqp

commit 49f395af059404e5752065913e779ee2425baa06

Update current client in `Upstream#read_loop`
Regression introduced by https://github.com/cloudamqp/amqproxy/pull/128

Close https://github.com/cloudamqp/amqproxy/issues/135

.github/workflows/ci.yml
@@ -2,6 +2,7 @@ name: CI
 on:
   push:
     paths:
+      - 'run-specs-in-docker.sh'
       - '.github/workflows/ci.yml'
       - 'shard.yml'
       - 'shard.lock'
@@ -12,28 +13,17 @@ on:
 jobs:
   spec:
     runs-on: ubuntu-latest
-    container: 84codes/crystal:latest-ubuntu-22.04
+    timeout-minutes: 10
     steps:
-      - name: Install RabbitMQ
-        run: apt-get update && apt-get install -y rabbitmq-server
-
-      - name: Start RabbitMQ
-        run: RABBITMQ_PID_FILE=/tmp/rabbitmq.pid rabbitmq-server -detached
-
       - name: Checkout
         uses: actions/checkout@v4
 
-      - name: Install shards
-        run: sed -i '/ameba/d' shard.yml && shards install
-
-      - name: Wait for RabbitMQ to start
-        run: rabbitmqctl wait /tmp/rabbitmq.pid
-
       - name: Run tests
-        run: crystal spec --order random
+        run: ./run-specs-in-docker.sh
 
   lint:
     runs-on: ubuntu-latest
+    timeout-minutes: 10
     container: 84codes/crystal:latest-ubuntu-22.04
     steps:
       - uses: actions/checkout@v4
@@ -42,6 +32,7 @@ jobs:
 
   format:
     runs-on: ubuntu-latest
+    timeout-minutes: 10
     container: 84codes/crystal:latest-ubuntu-22.04
     steps:
       - uses: actions/checkout@v4

run-specs-in-docker.sh
@@ -1,0 +1,13 @@
+#!/bin/bash
+
+set -x
+set -e
+
+docker compose \
+  --file spec/docker-compose.yml \
+  up \
+  --remove-orphans \
+  --force-recreate \
+  --renew-anon-volumes \
+  --build \
+  --exit-code-from spec

src/amqproxy/upstream.cr
@@ -40,7 +40,6 @@ module AMQProxy
     # Frames from upstream (to client)
     def read_loop # ameba:disable Metrics/CyclomaticComplexity
       socket = @socket
-      client = @current_client
       loop do
         AMQ::Protocol::Frame.from_io(socket, IO::ByteFormat::NetworkEndian) do |frame|
           case frame
@@ -56,7 +55,7 @@ module AMQProxy
             write frame
             next
           end
-          if client
+          if client = @current_client
             begin
               client.write(frame)
             rescue ex
@@ -85,7 +84,7 @@ module AMQProxy
       @log.error "Error reading from upstream: #{ex.inspect_with_backtrace}" unless @socket.closed?
     ensure
       @socket.close unless @socket.closed?
-      client.close_socket if client
+      @current_client.try &.close_socket
     end
 
     SAFE_BASIC_METHODS = {40, 10} # qos and publish

spec/Dockerfile
@@ -1,0 +1,16 @@
+FROM 84codes/crystal:latest-ubuntu-22.04
+
+RUN apt-get update && apt-get install -y rabbitmq-server
+
+WORKDIR /tmp
+
+# We want to install shards before copying code/spec files for quicker runs
+COPY shard.yml shard.lock ./
+RUN shards install
+
+COPY src/ src/
+COPY spec/ spec/
+
+COPY spec/entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]

spec/amqproxy_spec.cr
@@ -21,6 +21,44 @@ describe AMQProxy::Server do
     end
   end
 
+  it "publish and consume works" do
+    server = AMQProxy::Server.new("127.0.0.1", 5672, false, Logger::DEBUG)
+    begin
+      spawn { server.listen("127.0.0.1", 5673) }
+      Fiber.yield
+
+      queue_name = "amqproxy-test-queue"
+      num_received_messages = 0
+      num_messages_to_publish = 5
+
+      num_messages_to_publish.times do
+        AMQP::Client.start("amqp://localhost:5673") do |conn|
+          channel = conn.channel
+          queue = channel.queue(queue_name)
+          queue.publish_confirm("Message from AMQProxy specs")
+        end
+        sleep 0.1
+      end
+
+      AMQP::Client.start("amqp://localhost:5673") do |conn|
+        channel = conn.channel
+        channel.basic_consume(queue_name, tag: "AMQProxy specs") do |msg|
+          body = msg.body_io.to_s
+          if body == "Message from AMQProxy specs"
+            # FIXME: ack:ing causes this bug
+            # https://github.com/cloudamqp/amqproxy/issues/137
+            # channel.basic_ack(msg.delivery_tag)
+            num_received_messages += 1
+          end
+        end
+      end
+
+      num_received_messages.should eq num_messages_to_publish
+    ensure
+      server.stop_accepting_clients
+    end
+  end
+
   it "can reconnect if upstream closes" do
     s = AMQProxy::Server.new("127.0.0.1", 5672, false, Logger::DEBUG)
     begin

spec/docker-compose.yml
@@ -1,0 +1,6 @@
+version: "3.8"
+services:
+  spec:
+    build:
+      context: ..
+      dockerfile: spec/Dockerfile

spec/entrypoint.sh
@@ -1,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+
+export RABBITMQ_PID_FILE=/tmp/rabbitmq.pid
+
+# Start RabbitMQ
+rabbitmq-server -detached
+
+# Wait for RabbitMQ to start
+rabbitmqctl wait $RABBITMQ_PID_FILE
+
+crystal --version
+crystal spec --order random

spec/spec_helper.cr
@@ -4,3 +4,33 @@ require "../src/amqproxy/version"
 require "amqp-client"
 
 MAYBE_SUDO = (ENV.has_key?("NO_SUDO") || `id -u` == "0\n") ? "" : "sudo "
+
+# Spec timeout borrowed from Crystal project:
+# https://github.com/crystal-lang/crystal/blob/1.10.1/spec/support/mt_abort_timeout.cr
+
+private SPEC_TIMEOUT = 15.seconds
+
+Spec.around_each do |example|
+  done = Channel(Exception?).new
+
+  spawn(same_thread: true) do
+    begin
+      example.run
+    rescue e
+      done.send(e)
+    else
+      done.send(nil)
+    end
+  end
+
+  timeout = SPEC_TIMEOUT
+
+  select
+  when res = done.receive
+    raise res if res
+  when timeout(timeout)
+    _it = example.example
+    ex = Spec::AssertionFailed.new("spec timed out after #{timeout}", _it.file, _it.line)
+    _it.parent.report(:fail, _it.description, _it.file, _it.line, timeout, ex)
+  end
+end