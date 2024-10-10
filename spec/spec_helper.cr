a00bec6d94145b7624f631723158f1cee1375eff
Jon Börjesson <jon@spuun.se>
2024-10-10 10:04:10 +0200

    Make upstream url configurable via environment variable in spec

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,12 +1,20 @@
 require "spec"
+require "uri"
 require "../src/amqproxy/server"
 require "../src/amqproxy/version"
 require "amqp-client"
 
 MAYBE_SUDO = (ENV.has_key?("NO_SUDO") || `id -u` == "0\n") ? "" : "sudo "
 
+UPSTREAM_URL = begin
+  URI.parse ENV.fetch("UPSTREAM_URL", "amqp://127.0.0.1:5672?idle_connection_timeout=5")
+rescue e : URI::Error
+  puts "Invalid UPSTREAM_URL: #{e}"
+  exit 1
+end
+
 def with_server(idle_connection_timeout = 5, &)
-  server = AMQProxy::Server.new("127.0.0.1", 5672, false)
+  server = AMQProxy::Server.new(UPSTREAM_URL)
   tcp_server = TCPServer.new("127.0.0.1", 0)
   amqp_url = "amqp://#{tcp_server.local_address}"
   spawn { server.listen(tcp_server) }
@@ -29,9 +37,13 @@ def with_http_server(idle_connection_timeout = 5, &)
 end
 
 def verify_running_amqp!
-  TCPSocket.new("127.0.0.1", 5672, connect_timeout: 3.seconds).close
+  tls = UPSTREAM_URL.scheme == "amqps"
+  host = UPSTREAM_URL.host || "127.0.0.1"
+  port = UPSTREAM_URL.port || 5762
+  port = 5671 if tls && UPSTREAM_URL.port.nil?
+  TCPSocket.new(host, port, connect_timeout: 3.seconds).close
 rescue Socket::ConnectError
-  STDERR.puts "[ERROR] Specs require a running amqp server on 127.0.0.1:5672"
+  STDERR.puts "[ERROR] Specs require a running amqp server on #{host}:#{port}"
   exit 1
 end
 
