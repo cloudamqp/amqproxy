6026688731c689cb6b69eda8d382ca1d83aba63c
Viktor Erlingsson <viktor@84codes.com>
2024-10-23 10:24:35 +0200

    HTTP Health check and metrics (#184)
    Adds a health check over http on http://listen_address:http_port/healthz that returns 200 OK when amqproxy is up and running.
    
    Also adds some simple prometheus style metrics. Just client_connections and upstream_connections for now, but this can be extended in the future (maybe some throughput stats and server resource information?).

---
+++ spec/amqproxy/http_server_spec.cr
@@ -0,0 +1,37 @@
+require "../spec_helper"
+require "../../src/amqproxy/http_server"
+require "http/client"
+
+base_addr = "http://localhost:15673"
+
+describe AMQProxy::HTTPServer do
+  it "GET /healthz returns 200" do
+    with_http_server do
+      response = HTTP::Client.get("#{base_addr}/healthz")
+      response.status_code.should eq 200
+    end
+  end
+
+  it "GET /metrics returns 200" do
+    with_http_server do
+      response = HTTP::Client.get("#{base_addr}/metrics")
+      response.status_code.should eq 200
+    end
+  end
+
+  it "GET /metrics returns correct metrics" do
+    with_http_server do |_http, _amqproxy, proxy_url|
+      AMQP::Client.start(proxy_url) do |conn|
+        _ch = conn.channel
+        response = HTTP::Client.get("#{base_addr}/metrics")
+        response.body.split("\n").each do |line|
+          if line.starts_with?("amqproxy_client_connections")
+            line.should eq "amqproxy_client_connections 1"
+          elsif line.starts_with?("amqproxy_upstream_connections")
+            line.should eq "amqproxy_upstream_connections 1"
+          end
+        end
+      end
+    end
+  end
+end
---
+++ src/amqproxy/http_server.cr
@@ -0,0 +1,60 @@
+require "log"
+require "./prometheus_writer"
+require "http/server"
+
+module AMQProxy
+  class HTTPServer
+    Log = ::Log.for(self)
+
+    def initialize(amqproxy : Server, address : String, port : Int32)
+      @amqproxy = amqproxy
+      @address = address
+      @port = port
+      @http = HTTP::Server.new do |context|
+        case context.request.resource
+        when "/metrics"
+          metrics(context)
+        when "/healthz"
+          context.response.content_type = "text/plain"
+          context.response.print "OK"
+        else
+          context.response.respond_with_status(::HTTP::Status::NOT_FOUND)
+        end
+      end
+      bind_tcp
+      spawn @http.listen, name: "HTTP Server"
+      Log.info { "HTTP server listening on #{@address}:#{@port}" }
+    end
+
+    def bind_tcp
+      addr = @http.bind_tcp @address, @port
+      Log.info { "Bound to #{addr}" }
+    end
+
+    def metrics(context)
+      writer = PrometheusWriter.new(context.response, "amqproxy")
+      writer.write({name:   "identity_info",
+                    type:   "gauge",
+                    value:  1,
+                    help:   "System information",
+                    labels: {
+                      "#{writer.prefix}_version"  => AMQProxy::VERSION,
+                      "#{writer.prefix}_hostname" => System.hostname,
+                    }})
+      writer.write({name:  "client_connections",
+                    value: @amqproxy.client_connections,
+                    type:  "gauge",
+                    help:  "Number of client connections"})
+      writer.write({name:  "upstream_connections",
+                    value: @amqproxy.upstream_connections,
+                    type:  "gauge",
+                    help:  "Number of upstream connections"})
+
+      context.response.status = ::HTTP::Status::OK
+    end
+
+    def close
+      @http.try &.close
+    end
+  end
+end
