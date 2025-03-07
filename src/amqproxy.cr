43eac1a29fdc4fe48a9480a84556f07999f765ee
Jon Börjesson <jon@spuun.se>
2025-03-07 08:21:03 +0100

    Use execution context if -Dpreview_mt

--- src/amqproxy.cr
+++ src/amqproxy.cr
@@ -1,2 +1,10 @@
 require "./amqproxy/cli"
+
+{% if flag?(:preview_mt) && !parse_type("ExecutionContext").resolve? %}
+  {% if read_file("shard.yml").includes? "execution_context" %}
+    {% puts "using execution_context shard" %}
+    require "execution_context"
+  {% end %}
+{% end %}
+
 AMQProxy::CLI.new.run(ARGV)
