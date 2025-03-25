002d90a90f8112c352596766b3c032a24d1a827e
Jon Börjesson <jon@spuun.se>
2025-03-25 11:39:19 +0100

    Also print commit and compile-time flags

--- src/amqproxy.cr
+++ src/amqproxy.cr
@@ -1,14 +1,24 @@
 require "./amqproxy/cli"
 
-{% if flag?(:preview_mt) %}
-  {% unless parse_type("Fiber::ExecutionContext").resolve? %}
-    {% if read_file("shard.yml").includes? "execution_context" %}
-      {% puts "using execution_context shard" %}
-      require "execution_context"
-    {% end %}
+# {% if flag?(:preview_mt) %}
+#  {% unless parse_type("Fiber::ExecutionContext").resolve? %}
+#    {% if read_file("shard.yml").includes? "execution_context" %}
+#      {% puts "using execution_context shard" %}
+#      require "execution_context"
+#    {% end %}
+#  {% end %}
+# {% end %}
+#
+{% begin %}
+  {%
+    flags = [] of String
+    flags << "-Dpreview_mt" if flag?(:preview_mt)
+    flags << "-Dmt" if flag?(:mt)
+    flags << "-Dexecution_context" if flag?(:execution_context)
+    flags << "-Drelease" if flag?(:release)
+  %}
+
+  puts "Built with #{Crystal::VERSION} #{Crystal::BUILD_COMMIT} {{flags.join(" ").id}}"
+
   {% end %}
-{% end %}
-
-puts "Built with #{Crystal::VERSION}"
-
 AMQProxy::CLI.new.run(ARGV)
